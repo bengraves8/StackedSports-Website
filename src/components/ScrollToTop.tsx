b4c04263c1fde7a2693780c9fe23096d10c20f4e
bengraves8 <ben@stakd.co>
2025-03-06 21:42:30 -0600

    GA Update

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,11 +1,18 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    // Send pageview to GA4 with full URL information
+    ReactGA.send({
+      hitType: "pageview",
+      page: pathname + window.location.search,
+      title: document.title
+    });
   }, [pathname]);
 
   return null;
